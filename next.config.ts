ad0191e009912ce69a649e720c73d05c9373c678
irshadazka2415 <irshadazka2415@gmail.com>
2026-09-16 02:35:38 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
-  images: { unoptimized: true },
+
+  basePath: '/irshad-cinematic-portfolioo',
+  assetPrefix: '/irshad-cinematic-portfolioo/',
+
+  images: {
+    unoptimized: true,
+  },
 }
 
 export default nextConfig
